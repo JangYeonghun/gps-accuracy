116ad0cb3e799289f9901e35956944a932e204ee
JangYeonghun <gnsdudwkd@naver.com>
2023-08-10 14:47:02 +0900

    커스텀 마커 추가

--- lib/components/gpschecker.dart
+++ lib/components/gpschecker.dart
@@ -10,8 +10,8 @@ class GpsChecker extends StatefulWidget {
 }
 
 class _GpsCheckerState extends State<GpsChecker> {
-
   bool _pass = false;
+  bool _click = false;
 
   Future<void> check() async {
     Future<bool> _isEnabled = Geolocator.isLocationServiceEnabled();
@@ -24,13 +24,22 @@ class _GpsCheckerState extends State<GpsChecker> {
     });
   }
 
+  void click() {
+    _click = true;
+    check();
+    if (!_pass) {
+      _click = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _pass ? GpsModule() : AlertDialog(
+    check();
+    return _pass&&_click ? GpsModule() : AlertDialog(
       content: Text('실행에 위치서비스가 필요합니다.'),
       actions: [
         TextButton(onPressed: (){
-          check();
+          click();
         },
         child: Text('켰어요!'))
       ],
--- lib/components/map.dart
+++ lib/components/map.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:gps/provider/LatLngProvider.dart';
@@ -18,6 +21,20 @@ class _MapModuleState extends State<MapModule> {
   bool _showLog = false;
   Timer? _timer;
   final LatLng _center = LatLng(0, 0);
+
+  Uint8List? customMarkerIcon;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _loadCustomMarker();
+    });
+  }
+
+  void _loadCustomMarker() async {
+    customMarkerIcon = await getBytesFromAsset("assets/img/marker.png", 130); // 이미지 경로와 width 설정 필요
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -53,20 +70,20 @@ class _MapModuleState extends State<MapModule> {
                 setState(() {
                   _isTracking = value;
                 });
-              }
+              },
             ),
           ),
           Positioned(
-              bottom: 50,
-              left: 16,
-              child: Switch(
-                  value: _showLog,
-                  onChanged: (value) {
-                    setState(() {
-                      _showLog = value;
-                    });
-                  }
-              )
+            bottom: 50,
+            left: 16,
+            child: Switch(
+              value: _showLog,
+              onChanged: (value) {
+                setState(() {
+                  _showLog = value;
+                });
+              },
+            ),
           ),
           if (_showLog)
             Positioned(
@@ -80,22 +97,32 @@ class _MapModuleState extends State<MapModule> {
     );
   }
 
-
-
   Set<Marker> _createMarkers() {
     final LatLngProv gpsProvider = Provider.of<LatLngProv>(context, listen: true);
     final LatLng currentLatLng = LatLng(gpsProvider.Lat, gpsProvider.Lng);
 
-    final Marker currentMarker = Marker(
-      markerId: MarkerId('currentLocation'),
-      position: currentLatLng,
-    );
+    Marker? customMarker;
+
+    if (customMarkerIcon != null) {
+      customMarker = Marker(
+        markerId: MarkerId('customMarker'),
+        position: currentLatLng,
+        icon: BitmapDescriptor.fromBytes(customMarkerIcon!),
+        infoWindow: InfoWindow(title: '현재 위치', snippet: '이동중'),
+      );
+    }
 
     if (_isTracking) {
       _goToMyLocation();
     }
 
-    return {currentMarker};
+    final markers = <Marker>{};
+
+    if (customMarker != null) {
+      markers.add(customMarker);
+    }
+
+    return markers;
   }
 
   Future<void> _goToMyLocation() async {
@@ -104,5 +131,19 @@ class _MapModuleState extends State<MapModule> {
 
     mapController.animateCamera(CameraUpdate.newLatLngZoom(currentLatLng, 17));
   }
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
+    );
+    ui.FrameInfo fi = await codec.getNextFrame();
+    ByteData? byteData = await fi.image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
+  }
 }
+
+
+
 
