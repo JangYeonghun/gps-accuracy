c26abdc49377464de3b53b3fc238f702129c2799
3C37 <dooho0421@gmail.com>
2023-08-10 09:20:00 +0900

    gps check on app initiate bug fix

--- lib/components/gpschecker.dart
+++ lib/components/gpschecker.dart
@@ -11,6 +11,7 @@ class GpsChecker extends StatefulWidget {
 
 class _GpsCheckerState extends State<GpsChecker> {
   bool _pass = false;
+  bool _click = false;
 
   Future<void> check() async {
     Future<bool> _isEnabled = Geolocator.isLocationServiceEnabled();
@@ -23,14 +24,22 @@ class _GpsCheckerState extends State<GpsChecker> {
     });
   }
 
+  void click() {
+    _click = true;
+    check();
+    if (!_pass) {
+      _click = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     check();
-    return _pass ? GpsModule() : AlertDialog(
+    return _pass&&_click ? GpsModule() : AlertDialog(
       content: Text('실행에 위치서비스가 필요합니다.'),
       actions: [
         TextButton(onPressed: (){
-          check();
+          click();
         },
         child: Text('켰어요!'))
       ],
